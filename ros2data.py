11ff215ed0ab5b7d9c5e8ce95175253a148f9f25
ans92175@gmail.com <gymoon@inha.edu>
2024-02-13 16:02:21 +0900

    add file

---
+++ ros2data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import rospy
+from novatel_oem7_msgs.msg import INSPVA
+
+lat = 0
+lon = 0
+
+def inspva_callback(data):
+    lat = data.latitude
+    lon = data.longitude
+
+def listener():
+    rospy.init_node('inspva_listener', anonymous=True)
+    rospy.Subscriber("/novatel/oem7/inspva", INSPVA, inspva_callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
+    print(lat, lon)
