7bcb60d533f6cfc371cb31566cc30b1b6c7fc63e
cvlab_moon <ans92175@gmail.com>
2024-03-11 17:08:41 +0900

    add dataset evaluator

---
+++ dataset_distance_eval.py
@@ -0,0 +1,21 @@
+from gps2meter import gps_to_meter
+
+file_path = './data/test/gps.txt'
+
+gps_list = []
+
+with open(file_path, 'r') as file:
+    for line in file:
+        line = line.split()
+        lat = float(line[1])
+        lon = float(line[2])
+        gps_list.append((lat, lon))
+        lat = 0
+        lon = 0
+
+distance_sum = 0
+
+for i in range(len(gps_list) - 1):
+    distance_sum += gps_to_meter(gps_list[i][0], gps_list[i][1], gps_list[i+1][0], gps_list[i+1][1])
+    
+print(f'avr distance is {distance_sum/(len(gps_list) - 1)}')
