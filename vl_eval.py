aa483e7001b6a6928c2001d6d92d560db2dedd35
cvlab_moon <ans92175@gmail.com>
2024-03-12 23:03:48 +0900

    add evaluation code

--- vl_eval.py
+++ vl_eval.py
@@ -24,22 +24,66 @@ print('db gps list completes')
 iter = 1
 result = {}
 retrieved_list = []
+initial_query = '1114/000002.png'
 with open(result_file_path, 'r') as file:
     for line in file:
         line = line.split(', ')
         if line[0][0] == '#': continue
-        
-        if iter <= 5:
+
+        if initial_query != line[0][24:]:
+            initial_query = line[0][24:]
+            iter = 1
+
+        if initial_query == line[0][24:] and iter < 6:
             retrieved_list.append(line[1][24:])
-        else: pass
 
         if iter == 5:
             result[line[0][24:]] = retrieved_list
             retrieved_list = []
 
-        if iter == 101: iter = 0
+        # if iter == 101: iter = 0
 
         iter += 1
 
-# pprint.pprint(result)
-print(result['1114/000002.png'][0][-9:-5])
+pprint.pprint(result)
+# print(result['1114/000002.png'][0][-9:-5])
+
+# evaluation
+distances = []
+
+for key, value in result.items():
+    query_gps = query_gps_list[int(key[-8:-4]) - 2]
+    for i in value:
+        retrieved_gps = db_gps_list[int(i[-9:-5]) - 2]
+        distances.append(gps_to_meter(query_gps[0], query_gps[1], retrieved_gps[0], retrieved_gps[1]))
+
+top1 = []
+top2 = []
+top3 = []
+top4 = []
+top5 = []
+
+for i in range(len(distances)):
+    if i % 5 == 0:
+        top1.append(distances[i])
+    elif i % 5 == 1:
+        top2.append(distances[i])
+    elif i % 5 == 2:
+        top3.append(distances[i])
+    elif i % 5 == 3:
+        top4.append(distances[i])
+    elif i % 5 == 4:
+        top5.append(distances[i])
+
+arr = []
+arr.append(top1)
+arr.append(top2)
+arr.append(top3)
+arr.append(top4)
+arr.append(top5)
+
+for idx, val in enumerate(arr):
+    print(f'top{idx + 1} statistics')
+    print(f'min  : {min(val)}')
+    print(f'max  : {max(val)}')
+    print(f'mean : {sum(val)/len(val)}')
