6cd0bb86296e053d053d889b0a211d341d8b5646
cvlab_moon <ans92175@gmail.com>
2024-03-13 16:55:52 +0900

    add todo

--- pose_estimation_eval.py
+++ pose_estimation_eval.py
@@ -1,11 +1,15 @@
 from gps2meter import gps_to_meter
 from dataset_distance_eval import gps_reader
 
-tmp = {'b' : 2, 'c' : 3}
+def main():
+    '''
+    Todo list.
+    1. 쿼리 이미지 입력에 해당하는 image retrieval list
+    2. 쿼리 이미지 <-> retrieved image 사이 R|t
+    3. 쿼리 이미지 GPS, retrieved image GPS 사이 거리 계산(gt, scale)
+    4. 평가 코드
+    '''
+    pass
 
-def tmptmp(tmp):
-    tmp['a'] = 1
-
-tmptmp(tmp)
-
-print(tmp)
+if __name__ == '__main__':
+    main()
