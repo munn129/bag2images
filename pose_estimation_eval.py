acb67f25682377d2b3e61d4c6cb3934f37f7ccf4
cvlab_moon <ans92175@gmail.com>
2024-03-13 17:24:40 +0900

    code refactoring

--- pose_estimation_eval.py
+++ pose_estimation_eval.py
@@ -1,15 +1,39 @@
 from gps2meter import gps_to_meter
 from dataset_distance_eval import gps_reader
+from image_retrieval_eval import result_sorting
+from pose_recovery import image_rt_calculator
+
+'''
+Todo list.
+1. 쿼리 이미지 입력에 해당하는 image retrieval list
+2. 쿼리 이미지 <-> retrieved image 사이 R|t
+3. 쿼리 이미지 GPS, retrieved image GPS 사이 거리 계산(gt, scale)
+4. 평가 코드
+'''
 
 def main():
-    '''
-    Todo list.
-    1. 쿼리 이미지 입력에 해당하는 image retrieval list
-    2. 쿼리 이미지 <-> retrieved image 사이 R|t
-    3. 쿼리 이미지 GPS, retrieved image GPS 사이 거리 계산(gt, scale)
-    4. 평가 코드
-    '''
-    pass
+    
+    dataset = './data/1024_1m'
+    query = './data/1114'
+    result_file_path = f'{dataset}/PatchNetVLAD_predictions.txt'
+    gps_file_path = f'{dataset}/gps.txt'
+    query_gps_file_path = f'{query}/gps.txt'
+
+    # gps list index 0 -> 000002.png, 1 -> 000003.png, ...
+    # read query gps list
+    query_gps_list = []
+    gps_reader(query_gps_file_path, query_gps_list)
+    print('query gps list completes')
+
+    # read db gps list
+    db_gps_list = []
+    gps_reader(gps_file_path, db_gps_list)
+    print('db gps list completes')
+
+    # read image retrieval result
+    result = {}
+    retrieved_list = []
+    result_sorting(result_file_path, result, retrieved_list)
 
 if __name__ == '__main__':
     main()
--- pose_recovery.py
+++ pose_recovery.py
@@ -1,63 +1,75 @@
 import numpy as np
 import cv2
 
-from math import sqrt, pi, sin, cos, atan2
+from gps2meter import gps_to_meter
 
-def gps_to_meter(lat1, long1, lat2, long2) -> float:
-    R = 6378.137 # radius of the earth in KM
-    lat_to_deg = lat2 * pi/180 - lat1 * pi/180
-    long_to_deg = long2 * pi/180 - long1 * pi/180
+from math import sqrt
 
-    a = sin(lat_to_deg/2)**2 + cos(lat1 * pi/180) * cos(lat2 * pi/180) * sin(long_to_deg/2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1-a))
-    d = R * c
+def image_rt_calculator(query_image_path, dataset_image_path, rt_list):
     
-    return d * 1000 # meter
+    query_image = cv2.imread(query_image_path, cv2.IMREAD_GRAYSCALE)
+    dataset_image = cv2.imread(dataset_image_path, cv2.IMREAD_GRAYSCALE)
 
-image1 = cv2.imread('./000060.png', cv2.IMREAD_GRAYSCALE) # query
-image2 = cv2.imread('./000030.png', cv2.IMREAD_GRAYSCALE) # dataset
+    sift_obj = cv2.SIFT_create()
 
-sift_obj = cv2.SIFT_create()
+    query_keypoint, query_descriptor = sift_obj.detectAndCompute(query_image, None)
+    dataset_keypoint, dataset_descriptor = sift_obj.detectAndCompute(dataset_image, None)
 
-keypoint1, descriptor1 = sift_obj.detectAndCompute(image1, None)
-keypoint2, descriptor2 = sift_obj.detectAndCompute(image2, None)
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
 
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks = 50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(query_descriptor, dataset_descriptor, k = 2)
+    
+    query_points = []
+    dataset_points = []
 
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-matches = flann.knnMatch(descriptor1, descriptor2, k = 2)
+    for i, (m,n) in enumerate(matches):
+        if m.distance < 0.8 * n.distance:
+            # if m.distance is under 0.5, error occurs
+            dataset_points.append(dataset_keypoint[m.trainIdx].pt)
+            query_points.append(query_keypoint[m.queryIdx].pt)
 
-points1 = []
-points2 = []
+    query_points = np.int32(query_points)
+    dataset_points = np.int32(dataset_points)
 
-for i, (m,n) in enumerate(matches):
-    if m.distance < 0.8 * n.distance:
-        # if m.distance is under 0.5, error occurs
-        points2.append(keypoint2[m.trainIdx].pt)
-        points1.append(keypoint1[m.queryIdx].pt)
+    E, mask = cv2.findEssentialMat(query_points, dataset_points)
 
-points1 = np.int32(points1)
-points2 = np.int32(points2)
+    de_rot1, de_rot2, de_tran = cv2.decomposeEssentialMat(E)
+    re_retval, re_rot, re_tran, re_mask = cv2.recoverPose(E, query_points, dataset_points)
 
-E, mask = cv2.findEssentialMat(points1, points2)
+    rt_list.append(de_rot1)
+    rt_list.append(de_rot2)
+    rt_list.append(de_tran)
+    rt_list.append(re_rot)
+    rt_list.append(re_tran)
 
-de_rot1, de_rot2, de_tran = cv2.decomposeEssentialMat(E)
-re_retval, re_rot, re_tran, re_mask = cv2.recoverPose(E, points1, points2)
+def main():
+    query_image_path = './000060.png'
+    dataset_image_path = './000030.png'
+    rt_list = []
 
-query_gps = 37.396247679384764, 126.63717799601285
-data_gps = 37.39627776359797, 126.63713351147672
+    image_rt_calculator(query_image_path, dataset_image_path, rt_list)
 
-scale = sqrt((query_gps[0]-data_gps[0])**2 + (query_gps[1]-data_gps[1])**2)
-print(scale)
+    de_tran = rt_list[2]
+    re_tran = rt_list[4]
 
-estimate = data_gps[0] + (scale * re_tran[0]), data_gps[1] + (scale * re_tran[1])
-estimate2 = data_gps[0] + (scale * de_tran[0]), data_gps[1] + (scale * de_tran[1])
+    query_gps = 37.396247679384764, 126.63717799601285
+    data_gps = 37.39627776359797, 126.63713351147672
 
-distance = gps_to_meter(data_gps[0], data_gps[1], estimate[0], estimate[1])
-distance2 = gps_to_meter(data_gps[0], data_gps[1], estimate2[0], estimate2[1])
-gt = gps_to_meter(query_gps[0], query_gps[1], data_gps[0], data_gps[1])
+    scale = sqrt((query_gps[0]-data_gps[0])**2 + (query_gps[1]-data_gps[1])**2)
+    print(scale)
 
-print(f'decompose method: {distance}, error: {distance-gt}\n recover method: {distance2}, error: {distance2 - gt}\n\
-      gt: {gt}')
+    estimate = data_gps[0] + (scale * re_tran[0]), data_gps[1] + (scale * re_tran[1])
+    estimate2 = data_gps[0] + (scale * de_tran[0]), data_gps[1] + (scale * de_tran[1])
+
+    distance = gps_to_meter(data_gps[0], data_gps[1], estimate[0], estimate[1])
+    distance2 = gps_to_meter(data_gps[0], data_gps[1], estimate2[0], estimate2[1])
+    gt = gps_to_meter(query_gps[0], query_gps[1], data_gps[0], data_gps[1])
+
+    print(f'decompose method: {distance}, error: {distance-gt}\n recover method: {distance2}, error: {distance2 - gt}\n\
+        gt: {gt}')
+    
+if __name__ == '__main__':
+    main()
